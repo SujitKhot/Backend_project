bcff6b8f448ac283a75f08e71fdc531460cff8e3
sujit_khot <khotsujit2002@gmail.com>
2024-07-03 22:58:06 +0530

    config: express configuration

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -9,5 +10,13 @@ app.use(
     credentials: true,
   })
 );
+
+app.use(express.json({ limit: "16kb" }));
+
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+app.use(express.static("public"));
+
+app.use(cookieParser());
 
 export { app };
